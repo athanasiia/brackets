37dccfe0c55f21a113d8f029d7ca103ba4051c15
athanasiia <grvkan@gmail.com>
2023-09-01 15:53:17 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  bracketsConfig = [].concat(...bracketsConfig);
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    stack.push(str[i]);
+    if (bracketsConfig.lastIndexOf(str[i]) % 2 !== 0 && bracketsConfig[bracketsConfig.lastIndexOf(str[i]) - 1] === stack[stack.length - 2]) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0 ? true : false;
 }
